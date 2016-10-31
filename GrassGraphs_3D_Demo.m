9cf09d61675864004334065506555bce29a86f18
MarkMoTrin <mmoyou@my.fit.edu>
2016-10-31 11:01:41 -0400

    Update GrassGraphs_3D_Demo.m

--- GrassGraphs_3D_Demo.m
+++ GrassGraphs_3D_Demo.m
@@ -13,6 +13,9 @@
 % 
 %     You should have received a copy of the GNU General Public License
 %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%   
+%     mmoyou@my.fit.edu
+%     Information Characterization and Exploitation (ICE) Lab
 % ------------------------------------------------------------------------------
 
 % GrassGraphs 2D Affine Matching Algorithm. 
