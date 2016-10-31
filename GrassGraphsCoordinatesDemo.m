2484043cb67f77bc161602472e294dbac8e82917
MarkMoTrin <mmoyou@my.fit.edu>
2016-10-31 11:00:58 -0400

    Update GrassGraphsCoordinatesDemo.m

--- GrassGraphsCoordinatesDemo.m
+++ GrassGraphsCoordinatesDemo.m
@@ -1,3 +1,22 @@
+% {GrassGraph Algorithm, used to perform affine invariant feature matching.}
+%     Copyright (C) {2016}  {Mark Moyou}
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%   
+%     mmoyou@my.fit.edu
+%     Information Characterization and Exploitation (ICE) Lab
+% ------------------------------------------------------------------------------
 % GrassGraphs 2D Affine Matching Algorithm. 
 clear; clc; close all;
 
